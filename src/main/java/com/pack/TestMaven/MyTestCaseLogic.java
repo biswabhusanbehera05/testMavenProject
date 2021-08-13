6df037e3c52b199b916f5ce2a4c8641bd9d34505
unknown <biswabhusanbehera05@gmail.com>
2021-08-13 21:07:55 +0530

    ok

---
+++ src/main/java/com/pack/TestMaven/MyTestCaseLogic.java
@@ -0,0 +1,81 @@
+package com.pack.TestMaven;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class MyTestCaseLogic {
+
+	public void callingMylogic(WebDriver driver) throws BiffException, IOException, InterruptedException {
+		// System.setProperty("webdriver.chrome.driver",
+		// "C:\\Users\\Dell\\Downloads\\chrome Driver\\chromedriver.exe");
+
+		driver.get("https://www.facebook.com/");
+
+		Workbook wb = Workbook.getWorkbook(new File("C:\\Users\\Dell\\Desktop\\sample.xls"));
+
+		Sheet sh = wb.getSheet("abc");
+
+		BufferedReader br = new BufferedReader(new FileReader("E:\\Java practice\\locator.csv"));
+		String line = null;
+		WebElement unm = null;
+		WebElement pwd = null;
+		while ((line = br.readLine()) != null) {
+			String arr[] = line.split("\\|");
+			if (arr[0].equals("fb_username")) {
+				
+				unm = returnLocatorElement(driver, arr);
+			}
+
+			if (arr[0].equals("fb_pass")) {
+
+				pwd = returnLocatorElement(driver, arr);
+			}
+
+			if (unm != null && pwd != null) {
+
+				for (int i = 0; i < sh.getRows(); i++) {
+
+					// System.out.println("u name: "+sh.getCell(0,i).getContents());
+
+					// System.out.println("password: "+sh.getCell(1,i).getContents());
+
+					unm.sendKeys(sh.getCell(0, i).getContents());
+					pwd.sendKeys(sh.getCell(1, i).getContents());
+
+					Thread.sleep(2000);
+					pwd.clear();
+					unm.clear();
+
+				}
+			}
+		}
+
+		// WebElement unm = driver.findElement(By.cssSelector("input[id=\"email\"]"));
+		// WebElement pwd = driver.findElement(By.cssSelector("input[id=\"pass\"]"));
+
+	}
+
+	public WebElement returnLocatorElement(WebDriver driver, String arr[]) {
+
+		if (arr[2].equalsIgnoreCase("css"))
+			return driver.findElement(By.cssSelector(arr[1]));
+
+		else if (arr[2].equalsIgnoreCase("xpath"))
+			return driver.findElement(By.xpath(arr[1]));
+
+		else
+			return null;
+
+	}
+}
---
+++ src/main/java/com/pack/TestMaven/ReadExcel2.java
@@ -0,0 +1,70 @@
+package com.pack.TestMaven;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import jxl.read.biff.BiffException;
+//passed the data from the Excel to your application.
+
+public class ReadExcel2 {
+
+/*	public static void main(String[] args) throws BiffException, IOException, InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dell\\Downloads\\chrome Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		WebElement unm = driver.findElement(By.id("email"));	
+	    WebElement pwd = driver.findElement(By.id("pass"));
+		
+		Workbook wb = Workbook.getWorkbook(new File("C:\\Users\\Dell\\Desktop\\sample.xls"));
+
+		Sheet sh = wb.getSheet("abc");
+		
+		for(int i=0; i<sh.getRows() ; i++) { 
+			
+            //System.out.println("u name: "+sh.getCell(0,i).getContents());
+			
+			//System.out.println("password: "+sh.getCell(1,i).getContents());
+			
+	    
+	    unm.sendKeys(sh.getCell(0,i).getContents());
+	    pwd.sendKeys(sh.getCell(1,i).getContents());
+	    
+	    Thread.sleep(2000);
+	    pwd.clear();
+	    unm.clear();
+	    
+		}
+
+	}*/
+
+	 WebDriver driver=null;
+	@Test(testName="My_testcase")
+	public void myTestcase() throws BiffException, IOException, InterruptedException
+	{
+		System.out.println("test");
+		MyTestCaseLogic obj = new MyTestCaseLogic();
+		obj.callingMylogic(driver);
+	}
+	@BeforeTest
+	public void loadDriver()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dell\\Downloads\\chrome Driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		System.out.println("before test");
+	}
+	
+	@AfterTest
+	public void closeDriver()
+	{
+		System.out.println("after test");
+		driver.close();
+	}
+
+}
