1289ae0cf5d23531dd906acc2e7b9fbbbd8caa02
biswabhusanbehera05 <biswabhusanbehera05@gmail.com>
2021-08-13 22:07:03 +0530

    Update App.java

--- src/main/java/com/pack/TestMaven/App.java
+++ src/main/java/com/pack/TestMaven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-		System.out.println( "Hello World war 2!" );
+		System.out.println( "Hello World war 212121212221!" );
         System.out.println( "Hello World!" );
     }
 }
